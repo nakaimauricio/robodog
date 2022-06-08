5ff31ee7310a26a73c96503240a9f05326b6fa4d
Mauricio Eiji Nakai <nakaimauricio@gmail.com>
2022-06-08 12:34:49 -0300

    Create inv_kin.m

---
+++ inv_kin.m
@@ -0,0 +1,44 @@
+% Cinemática Inversa
+classdef inv_kin
+    properties 
+        motor1
+        motor2
+    end
+    properties (Constant)
+        L1=69
+        L2=47
+    end
+    methods
+        function ang = calc_ang(obj,x,y)
+            %cinemática inversa
+            %x e y são representa a posição em relação ao ombro do robô
+            theta2rad = acos((x^2+y^2-obj.L1^2-obj.L2^2)/(2*obj.L1*obj.L2));
+            theta1rad = atan(y/x)-atan(obj.L2*sin(theta2rad)/(obj.L1+obj.L2*cos(theta2rad)));
+
+            ang.motor1 = 90-(theta1rad*180/pi);
+            ang.motor2 = 90-(theta2rad*180/pi);
+        end
+        function pos = calc_pos(obj,angmotor1,angmotor2)
+            %cinemática direta
+            %q1 ângulo do ombro do robo, posição 0 no meio da articulação
+            %q2 ângluo do joelho do robô, posição 0 com o joelho flexionado
+            %em 90 graus
+        
+            theta1=angmotor1;
+            theta2=90-angmotor2;
+            theta1rad=pi/180*theta1;
+            theta2rad=pi/180*theta2;
+        
+            %posição em Y
+            posY2=cos(theta1rad)*obj.L1 + cos(theta2rad-theta1rad)*obj.L2;
+            posY1=cos(theta1rad)*obj.L1;
+            
+            %posição em x
+            posX2 = sin(theta1rad)*obj.L1 - sin(theta2rad-theta1rad)*obj.L2;
+            posX1= sin(theta1rad)*obj.L1;
+            
+            line([0 posX1 posX2],[0 -posY1 -posY2])
+            grid
+        end
+    end
+end
