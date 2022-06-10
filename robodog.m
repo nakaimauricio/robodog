3cea490068c39de11652401ec179dd1af554bc08
Mauricio Eiji Nakai <nakaimauricio@gmail.com>
2022-06-10 12:06:41 -0300

    Update robodog.m

--- robodog.m
+++ robodog.m
@@ -1,13 +1,13 @@
-pata_dia_dir = inv_kin()
-pata_dia_esq = inv_kin()
-pata_tra_dir = inv_kin()
-pata_tra_esq = inv_kin()
+pata_dia_dir = inv_kin(0,100)
+% pata_dia_esq = inv_kin(0,100)
+% pata_tra_dir = inv_kin(0,100)
+% pata_tra_esq = inv_kin(0,100)
 % 
 % while
 %     
 % end
 
-motor=pata_dia_dir.calc_ang(0,100)
-pata_dia_dir.calc_pos(motor.motor1, motor.motor2,1)
-pata_dia_dir.calc_pos(motor.motor1, motor.motor2,2)
-pata_dia_dir.calc_pos(motor.motor1, motor.motor2,4)
+pata_dia_dir.move(0,90,1);
+pata_dia_dir.move(30,90,1);
+pata_dia_dir.move(30,100,1);
+pata_dia_dir.move(-30,90,1);
