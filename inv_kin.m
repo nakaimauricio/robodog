dc49b48910c7f6dde0b5fd7e26b356641b9fd0e7
Mauricio Eiji Nakai <nakaimauricio@gmail.com>
2022-06-10 12:07:29 -0300

    Update inv_kin.m

--- inv_kin.m
+++ inv_kin.m
@@ -3,12 +3,18 @@ classdef inv_kin
     properties 
         motor1
         motor2
+        atual_x
+        atual_y
     end
     properties (Constant)
         L1=69
         L2=47
     end
     methods
+        function obj = inv_kin(atual_x, atual_y)
+            obj.atual_x = atual_x;
+            obj.atual_y = atual_y;
+        end
         function ang = calc_ang(obj,x,y)
             %cinemática inversa
             %x e y são representa a posição em relação ao ombro do robô
@@ -17,8 +23,11 @@ classdef inv_kin
 
             ang.motor1 = 90-(theta1rad*180/pi);
             ang.motor2 = 90-(theta2rad*180/pi);
+            if (ang.motor1>180)
+                ang.motor1=ang.motor1-180;
+            end
         end
-        function pos = calc_pos(obj,angmotor1,angmotor2)
+        function pos = calc_pos(obj,angmotor1,angmotor2,janela)
             %cinemática direta
             %q1 ângulo do ombro do robo, posição 0 no meio da articulação
             %q2 ângluo do joelho do robô, posição 0 com o joelho flexionado
@@ -37,8 +46,29 @@ classdef inv_kin
             posX2 = sin(theta1rad)*obj.L1 - sin(theta2rad-theta1rad)*obj.L2;
             posX1= sin(theta1rad)*obj.L1;
             
+            figure(janela)
             line([0 posX1 posX2],[0 -posY1 -posY2])
             grid
+        end
+        function atual_x = get.atual_x(obj)
+        end
+        function move(obj, pos_x, pos_y, janela)
+            %movimentação contínua
+            erro=1
+            while (abs(obj.atual_x-pos_x)>erro || abs(obj.atual_y-pos_y)>erro)
+                if(pos_x-obj.atual_x>1)
+                    obj.atual_x = obj.atual_x+1;
+                elseif(pos_x-obj.atual_x<-1)
+                    obj.atual_x = obj.atual_x-1;
+                end
+                if(pos_y-obj.atual_y>1)
+                    obj.atual_y = obj.atual_y+1;
+                elseif(pos_y-obj.atual_y<-1)
+                    obj.atual_y = obj.atual_y-1;
+                end
+                motor=obj.calc_ang(obj.atual_x, obj.atual_y)
+                obj.calc_pos(motor.motor1,motor.motor2,janela)
+            end
         end
     end
 end
